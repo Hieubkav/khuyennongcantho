92bf017f6c5b215ee3eade627292a344e04ff7e5
Noah Hill <noahhill576@aol.com>
2025-09-10 01:33:49 +0700

    feat(docs): add plan documentation to docs collection

--- apps/web/.source/index.ts
+++ apps/web/.source/index.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck -- skip type checking
+import * as docs_1 from "../content/docs/plan.mdx?collection=docs&hash=1757429709189"
 import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1757429709189"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"E:/NextJS/persional_project/khuyennong/apps/web/content/docs/index.mdx"}, data: docs_0 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"E:/NextJS/persional_project/khuyennong/apps/web/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"plan.mdx","absolutePath":"E:/NextJS/persional_project/khuyennong/apps/web/content/docs/plan.mdx"}, data: docs_1 }], [])
